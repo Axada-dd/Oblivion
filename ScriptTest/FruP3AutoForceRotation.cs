007ef06b5414b07b482c525f4e13f745217efbbb
denglang <99187696@qq.com>
2025-06-12 00:51:05 +0800

    feat: 新增自动朝向光源功能

---
+++ ScriptTest/FruP3AutoForceRotation.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Numerics;
+using AEAssist;
+using AEAssist.CombatRoutine.Trigger;
+using AEAssist.CombatRoutine.Trigger.Node;
+using AEAssist.Helper;
+using AEAssist.MemoryApi;
+
+namespace Oblivion.ScriptTest;
+
+public class FruP3AutoForceRotation : ITriggerScript
+{
+    public bool Check(ScriptEnv scriptEnv, ITriggerCondParams condParams)
+    {
+        if (scriptEnv.KV.TryGetValue("lightPosition", out var lightPositionObj))
+        {
+            string direction = "";
+
+            Vector3 lightPosition = (Vector3)lightPositionObj;
+
+            float angle = MathF.Atan2(-(lightPosition.X - 100), -(lightPosition.Z - 100)); // 北为0，逆时针为正
+            angle = NormalizeAngle(angle); // 归一化到 [-π, π]
+            Core.Resolve<MemApiMove>().SetRot(angle);
+            LogHelper.Print($"Auto face to {direction}，Rotation: {angle}");
+            return true;
+        }
+
+        return false;
+    }
+
+    private static float NormalizeAngle(float angle)
+    {
+        while (angle <= -MathF.PI) angle += 2 * MathF.PI;
+        while (angle > MathF.PI) angle -= 2 * MathF.PI;
+        return angle;
+    }
+}
