646855352fa32feecb745bb4dca676b43c83e257
denglang <99187696@qq.com>
2025-06-12 00:09:50 +0800

    feat: 新增BLM Buffs类

---
+++ BLM/SlotResolver/Data/Buffs.cs
@@ -0,0 +1,14 @@
+namespace Oblivion.BLM.SlotResolver.Data;
+
+public class Buffs
+{
+    public const uint
+        雷云 = 3870,
+        黑魔纹Buff = 738,
+        即刻Buff = 167,
+        三连Buff = 1211,
+        火苗 = 165,
+        沉稳Buff = 160,
+        黑盾 = 168,
+        醒梦Buff = 1204;
+}
