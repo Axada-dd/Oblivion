546e02cbea1c0443b290540b562b71725d8e6f33
denglang <99187696@qq.com>
2025-06-12 15:20:54 +0800

    feat(ui): 添加说明标签和更新日志按钮

--- BLM/QtUI/ReadmeTab.cs
+++ BLM/QtUI/ReadmeTab.cs
@@ -1,6 +1,37 @@
+using System.Numerics;
+using AEAssist.CombatRoutine.View.JobView;
+using AEAssist.GUI;
+using Dalamud.Interface.Colors;
+using Dalamud.Utility;
+using ImGuiNET;
+
 namespace Oblivion.BLM.QtUI;
 
 public class ReadmeTab
 {
-    
+    private static readonly InfoBox Box = new()
+    {
+        AutoResize = true,
+        BorderColor = ImGuiColors.ParsedGold,
+        ContentsAction = () =>
+        {
+            if (ImGui.Button("查看更新日志"))
+            {
+                Util.OpenLink("https://github.com/yoyokity/AEACR/releases"); 
+            }
+            
+            ImGuiHelper.Separator();
+            ImGui.BulletText("Test");
+        }
+    };
+    public static void Build(JobViewWindow instance)
+    {
+        instance.AddTab("说明", window =>
+        {
+            ImGui.Dummy(new Vector2(0, 1));
+            ImGui.Dummy(new Vector2(5, 0));
+            ImGui.SameLine();
+            Box.DrawStretched();
+        });
+    }
 }
