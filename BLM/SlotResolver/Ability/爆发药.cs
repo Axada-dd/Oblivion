f691aa5e18a5df28ee18ad2f623fc8cd72d93851
denglang <99187696@qq.com>
2025-06-13 01:06:41 +0800

    fix(BLM): 增加爆发药检查逻辑

--- BLM/SlotResolver/Ability/爆发药.cs
+++ BLM/SlotResolver/Ability/爆发药.cs
@@ -1,5 +1,7 @@
 using AEAssist.CombatRoutine;
 using AEAssist.CombatRoutine.Module;
+using AEAssist.Helper;
+using Oblivion.BLM.QtUI;
 
 namespace Oblivion.BLM.SlotResolver.Ability;
 
@@ -7,7 +9,9 @@ public class 爆发药:ISlotResolver
 {
     public int Check()
     {
-        return 0;
+        if(!QT.Instance.GetQt("爆发药"))return -2; 
+        if (!ItemHelper.CheckCurrJobPotion()) return -3;
+        return -1;
     }
 
     public void Build(Slot slot)
