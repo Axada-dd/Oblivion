b587eb6d8acaeb39a27165cb60b872d42b4f8dde
denglang <99187696@qq.com>
2025-06-12 15:21:01 +0800

    feat(ui): 增加JobViewWindow实例化与构建逻辑

--- BLM/QtUI/QT.cs
+++ BLM/QtUI/QT.cs
@@ -1,6 +1,29 @@
+using AEAssist.CombatRoutine.View.JobView;
+using AEAssist.CombatRoutine.View.JobView.HotkeyResolver;
+
 namespace Oblivion.BLM.QtUI;
 
 public class QT
 {
-    
+    public static JobViewWindow Instance { get; set; }
+
+    /// <summary>
+    /// 除了爆发药以外都复原
+    /// </summary>
+    public static void Reset()
+    {
+        
+    }
+    public static void Build()
+    {
+        Instance = new JobViewWindow(BLMSetting.Instance.JobViewSave, BLMSetting.Instance.Save,"嗨呀黑魔");
+        
+        
+        Instance.AddQt("爆发药", false);
+        
+        Instance.AddHotkey("爆发药", new HotKeyResolver_Potion());
+        
+        ReadmeTab.Build(Instance);
+        SettingTab.Build(Instance);
+    }
 }
