1f9ef3578b5f3ae442aa1c008a535b50a5d01cf6
denglang <99187696@qq.com>
2025-06-12 15:20:36 +0800

    feat: 新增火三技能解析逻辑

---
+++ BLM/SlotResolver/GCD/火三.cs
@@ -0,0 +1,34 @@
+using AEAssist;
+using AEAssist.CombatRoutine;
+using AEAssist.CombatRoutine.Module;
+using AEAssist.Extension;
+using AEAssist.Helper;
+using Oblivion.BLM.SlotResolver.Data;
+using Oblivion.Common;
+
+namespace Oblivion.BLM.SlotResolver.GCD;
+
+public class 火三:ISlotResolver
+{
+    public int Check()
+    {
+        int nearbyEnemyCount = TargetHelper.GetNearbyEnemyCount(Core.Me.GetCurrTarget(), 25, 5);
+        if (!new Spell(154, SpellTargetType.Target).IsReadyWithCanCast()) return -2;
+        if (nearbyEnemyCount >= 3) return -3;
+        if(Core.Me.Level<60)return -4;
+        if(BLMHelper.火状态&&!Core.Me.HasAura(Buffs.火苗))return -5;
+        if (BLMHelper.火状态 && Core.Me.HasAura(Buffs.火苗) && BLMHelper.火层数 < 3) return 3;
+        if(BLMHelper.冰状态&&!Core.Me.HasAura(Buffs.火苗)&&BLMHelper.火层数>=3&&BLMHelper.冰针>=3&&Core.Me.CurrentMp>=10000)return 1;
+        if(BLMHelper.火状态&&Core.Me.HasAura(Buffs.火苗)&&BLMHelper.天语剩余时间<3000)return 4;
+        if(!BLMHelper.火状态&&!BLMHelper.冰状态&&Core.Me.CurrentMp>=10000)return 2;
+        return -1;
+    }
+
+    private Spell GetSpell() => 154U.GetActionChange().GetSpell();
+    public void Build(Slot slot)
+    {
+        Spell spell = this.GetSpell();
+        if (spell == null) return;
+        slot.Add(spell);
+    }
+}
