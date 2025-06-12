0689d02d4bb5e024293e5015ec214fd900a56d21
denglang <99187696@qq.com>
2025-06-12 15:21:25 +0800

    feat: 实现BLMEvetHandle接口方法

--- BLM/BLMEvetHandle.cs
+++ BLM/BLMEvetHandle.cs
@@ -1,6 +1,53 @@
+using System.Threading.Tasks;
+using AEAssist.CombatRoutine;
+using AEAssist.CombatRoutine.Module;
+
 namespace Oblivion.BLM;
 
-public class BLMEvetHandle
+public class BLMEvetHandle:IRotationEventHandler
 {
-    
+    public Task OnPreCombat()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnResetBattle()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public Task OnNoTarget()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnSpellCastSuccess(Slot slot, Spell spell)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void AfterSpell(Slot slot, Spell spell)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnBattleUpdate(int currTimeInMs)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnEnterRotation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnExitRotation()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnTerritoryChanged()
+    {
+        throw new System.NotImplementedException();
+    }
 }
